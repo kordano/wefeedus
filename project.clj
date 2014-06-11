d104bee7f39d689f284860696dcd890ff35cb2e0
Christian Weilbach <christian_weilbach@web.de>
2014-06-11 23:21:25 +0200

    Remove patched core.async dependency.

--- project.clj
+++ project.clj
@@ -9,7 +9,6 @@
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2227"]
-                 [org.clojure/core.async "0.1.0-SNAPSHOT"]
 
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail javax.jms/jms com.sun.jdmk/jmxtools com.sun.jmx/jmxri]]
                  [org.slf4j/slf4j-log4j12 "1.7.6"]
